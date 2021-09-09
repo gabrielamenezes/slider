6fc2f643021827810bebf5cb467490f0394218c0
Gabriela Menezes <64161478+gabrielamenezes@users.noreply.github.com>
2021-09-09 16:15:40 -0300

    add handleScrollFunction

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const previousEl = document.getElementById('previous')
 const nextEl = document.getElementById('next')
 const sliderEl = document.getElementById('slider')
+let interval = undefined;
+let timeout = undefined
 
 previousEl.addEventListener('click', onPreviousClick);
 nextEl.addEventListener('click', onNextClick);
@@ -10,18 +12,29 @@ autoScroll();
 function onPreviousClick() {
     const sliderWidth = sliderEl.offsetWidth; /* retorna largura do elemento*/
     sliderEl.scrollLeft -= sliderWidth;
+    handleSliderClick()
 }
 
 function onNextClick() {
     const sliderWidth = sliderEl.offsetWidth; /* retorna largura do elemento*/
     sliderEl.scrollLeft += sliderWidth;
+    handleSliderClick()
 }
-
+function handleSliderClick() {
+    clearTimeout(timeout)
+    clearInterval(interval)
+    interval = undefined
+    timeout = setTimeout( () => {
+        autoScroll()
+    }, 10000)
+}
 function autoScroll() {
-    setInterval( () => {
+    if(interval) return;
+
+    interval = setInterval( () => {
         const sliderWidth = sliderEl.offsetWidth;
         const numberOfImages = sliderEl.childElementCount;
-        const selectedImage = sliderEl.scrollLeft/sliderWidth;
+        const selectedImage = (sliderEl.scrollLeft/sliderWidth) + 1;
 
         //se for a última imagem -> volta pro 0
         if(selectedImage === numberOfImages) {
